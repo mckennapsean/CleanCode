421ff74eae979cea10a9859d2b99cd8c59abb914
Sean McKenna <mckennapsean@gmail.com>
2025-02-09 22:40:52 -0700

    interim

--- src/clean/code/chapter15/iterative/ComparisonCompactor.java
+++ src/clean/code/chapter15/iterative/ComparisonCompactor.java
@@ -1,52 +1,52 @@
-package clean.code.chapter15.defactored;
-
-import clean.code.chapter15.Assert;
+package clean.code.chapter15.interim;
 
 public class ComparisonCompactor {
-  private int ctxt;
-  private String s1;
-  private String s2;
-  private int pfx;
-  private int sfx;
-
-  public ComparisonCompactor(int ctxt, String s1, String s2) {
-    this.ctxt = ctxt;
-    this.s1 = s1;
-    this.s2 = s2;
-  }
-
-  public String compact(String msg) {
-    if (s1 == null || s2 == null || s1.equals(s2))
-      return Assert.format(msg, s1, s2);
-
-    pfx = 0;
-    for (; pfx < Math.min(s1.length(), s2.length()); pfx++) {
-      if (s1.charAt(pfx) != s2.charAt(pfx))
+/*
+  ...
+  private int suffixLength;
+...
+  private void findCommonPrefixAndSuffix() {
+    findCommonPrefix();
+    suffixLength = 0;
+    for (; !suffixOverlapsPrefix(suffixLength); suffixLength++) {
+      if (charFromEnd(expected, suffixLength) !=
+          charFromEnd(actual, suffixLength))
         break;
     }
-    int sfx1 = s1.length() - 1;
-    int sfx2 = s2.length() - 1;
-    for (; sfx2 >= pfx && sfx1 >= pfx; sfx2--, sfx1--) {
-      if (s1.charAt(sfx1) != s2.charAt(sfx2))
-        break;
-    }
-    sfx = s1.length() - sfx1;
-    String cmp1 = compactString(s1);
-    String cmp2 = compactString(s2);
-    return Assert.format(msg, cmp1, cmp2);
   }
 
-  private String compactString(String s) {
+  private char charFromEnd(String s, int i) {
+    return s.charAt(s.length() - i - 1);
+  }
+
+  private boolean suffixOverlapsPrefix(int suffixLength) {
+    return actual.length() - suffixLength <= prefixLength ||
+      expected.length() - suffixLength <= prefixLength;
+  }
+
+...
+  private String compactString(String source) {
     String result =
-      "[" + s.substring(pfx, s.length() - sfx + 1) + "]";
-    if (pfx > 0)
-      result = (pfx > ctxt ? "..." : "") +
-        s1.substring(Math.max(0, pfx - ctxt), pfx) + result;
-    if (sfx > 0) {
-      int end = Math.min(s1.length() - sfx + 1 + ctxt, s1.length());
-      result = result + (s1.substring(s1.length() - sfx + 1, end) +
-        (s1.length() - sfx + 1 < s1.length() - ctxt ? "..." : ""));
-    }
+      DELTA_START +
+      source.substring(prefixLength, source.length() - suffixLength) +
+      DELTA_END;
+    if (prefixLength > 0)
+      result = computeCommonPrefix() + result;
+    if (suffixLength > 0)
+      result = result + computeCommonSuffix();
     return result;
   }
+
+...
+  private String computeCommonSuffix() {
+    int end = Math.min(expected.length() - suffixLength +
+      contextLength, expected.length()
+    );
+    return
+      expected.substring(expected.length() - suffixLength, end) +
+      (expected.length() - suffixLength <
+        expected.length() - contextLength ?
+        ELLIPSIS : "");
+  }
+  */
 }
